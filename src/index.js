23669a8d40021b9009880f00276ca4b3942d944c
Khushipandey19 <khushipandey1231@gmail.com>
2025-07-05 14:19:01 +0530

    useEffeect

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5}/>
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    /> */}
     {/* <StarRating maxRating={10}/>
     <StarRating/> */}
+    {/* <StarRating size={24} color="red" defaultRating={4} /> */}
   </React.StrictMode>
 );
